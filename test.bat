ccdaa554b6b4b32142dfa94b9edf05642ba7c8f6
Villoro <villoro7@gmail.com>
2019-03-04 22:03:07 +0100

    [NEW] change styles + increase dpi to fix char spacing

--- test.bat
+++ test.bat
@@ -1,7 +1,10 @@
 :loop
 
 "C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"^
- --disable-smart-shrinking --javascript-delay 3000 -T 0 -B 0 -L 0 -R 0^
+ --disable-smart-shrinking^
+ --javascript-delay 10000^
+ --print-media-type --dpi 300^
+ -T 0 -B 0 -L 0 -R 0^
  test.html^
  test.pdf
 
