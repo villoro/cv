62bc1449c09bd9c06e128d6e2dc95056b3aa309c
Villoro <villoro7@gmail.com>
2019-03-05 18:24:01 +0100

    [NEW] Clarify the do_all script

--- src/do_all.py
+++ src/do_all.py
@@ -6,22 +6,26 @@ from subprocess import call
 import config as c
 
 WKHTML_CONFIG = {
-    "--javascript-delay": 5000,
+    "--javascript-delay": 5000,  # With a high number we allow the fonts to load
+    "--dpi": 300,  # Having this number high prevents resolution problems
     "--disable-smart-shrinking": "",
     "--print-media-type": "",
-    "--dpi": 300,
     "-T": 0,
     "-B": 0,
     "-L": 0,
     "-R": 0,
 }
 
+# PATH_WKHTML needs to be around " "
+# The other part is a concat of all options
 RAW_COMAND = [f'"{c.PATH_WKHTML}"'] + [f"{i} {x}".strip() for i, x in WKHTML_CONFIG.items()]
 
 
 def main():
 
+    # Do all in input folder
     for filename in os.listdir(c.PATH_INPUT):
+
         # No extension if present
         name = filename.split(".")[0]
 
