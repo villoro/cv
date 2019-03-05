271a071d092503a8191421942ffaef171202598f
Villoro <villoro7@gmail.com>
2019-03-05 11:43:12 +0100

    [NEW] sidebar skills

--- test.bat
+++ test.bat
@@ -2,7 +2,7 @@
 
 "C:\Program Files\wkhtmltopdf\bin\wkhtmltopdf.exe"^
  --disable-smart-shrinking^
- --javascript-delay 10000^
+ --javascript-delay 5000^
  --print-media-type --dpi 300^
  -T 0 -B 0 -L 0 -R 0^
  test.html^
