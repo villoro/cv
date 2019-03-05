80ab2ce8e35135313b863378253b668a15f3c675
Villoro <villoro7@gmail.com>
2019-03-05 13:47:47 +0100

    [NEW] add a frame for previewing with flask

--- src/index.py
+++ src/index.py
@@ -1,5 +1,5 @@
 """
-	Loads CV as a web for testing purpouses
+    Loads CV as a web for testing purpouses
 """
 
 from flask import Flask, render_template
@@ -9,13 +9,24 @@ from utilities import get_content, get_config
 APP = Flask(__name__)
 
 
-@APP.route("/")
-def home():
+@APP.route("/print.html")
+def print():
+    """ Display CV without any frame for printing """
 
     content = get_content()
     config = get_config()
 
     return render_template("cv.html", config=config, **content)
+
+
+@APP.route("/")
+def preview():
+    """ Render the CV before converting to html """
+
+    content = get_content()
+    config = get_config()
+
+    return render_template("cv.html", preview=True, config=config, **content)
 
 
 if __name__ == "__main__":
