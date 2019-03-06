e85733030cb7d2870530a5bfc29a2c520514a970
Villoro <villoro7@gmail.com>
2019-03-06 12:03:18 +0100

    [NEW] add links + markdown in descriptions

--- src/utilities.py
+++ src/utilities.py
@@ -1,6 +1,7 @@
 """ Load data from yamls as ordered dicts """
 
 import oyaml as yaml
+from markdown import markdown
 
 import config as c
 
@@ -10,6 +11,16 @@ def _read_yaml(uri):
 
     with open(uri, encoding="utf-8") as file:
         return yaml.load(file)
+
+
+def _transform_from_markdown(data):
+    """ Transform markdown text to html """
+
+    for block_name, block_data in data["body"].items():
+        for x in block_data:
+            x["description"] = markdown(x["description"])
+
+    return data
 
 
 def get_content(name=None):
@@ -26,6 +37,9 @@ def get_content(name=None):
 
     # Read data
     out = _read_yaml(f"{path}{name}.yaml")
+
+    # Transform description from markdown
+    out = _transform_from_markdown(out)
 
     # Add config data
     out["config"] = _read_yaml(c.FILE_CONFIG)
