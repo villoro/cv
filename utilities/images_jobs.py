b0d37e770465302b86472713fed6f979950efe59
villoro7 <villoro7@gmail.com>
2023-07-03 22:42:31 +0200

    Use more pydantic for jobs

--- utilities/images_jobs.py
+++ utilities/images_jobs.py
@@ -1,6 +1,9 @@
 import os
 
 from pydantic import BaseModel, validator, root_validator
+from typing import Literal, Union
+from tqdm import tqdm
+from vpalette import get_colors
 
 from image_transformations import (
     get_image,
@@ -9,144 +12,161 @@ from image_transformations import (
     add_background_color,
     round_image,
 )
-from vpalette import get_colors
 
 
-def get_folder_out_and_name(path_in):
-    """Gets the path where data should be stored and the base name"""
+class RoundImage(BaseModel):
+    action: Literal["round"]
+    suffix: str = "_round"
 
-    aux = path_in.split("/")
-
-    name = aux[-1].split(".")[0]
-    folder_in = "/".join(aux[:-1])
-
-    folder_out = f"{folder_in}/{name}"
-
-    if not os.path.exists(folder_out):
-        os.makedirs(folder_out)
-
-    return folder_out, name
+    def run(self, image_in):
+        return round_image(image_in)
 
 
-class ImageJobConfig(BaseModel):
-    """Config for jobs that process images"""
+class RemoveBg(BaseModel):
+    action: Literal["remove_bg"]
+    suffix: str = "_no_bg"
+    alpha_matting: bool = True
+    alpha_matting_erode_size = 40
 
-    background: bool = False
-    round: bool = False
+    def run(self, image_in):
+        return remove_bg(
+            image_in,
+            alpha_matting=self.alpha_matting,
+            alpha_matting_erode_size=self.alpha_matting_erode_size,
+        )
+
+
+class AddColorGeneric(BaseModel):
     color: str = None
     color_name: str = None
     color_index: int = None
-    grayscale: bool = False
 
-    @root_validator
-    def populate_color(cls, values):
+    @validator("color", pre=True, always=True)
+    def populate_color(cls, v, values):
+        """Infer color form color_name and color_index"""
 
         color_name = values.get("color_name")
         color_index = values.get("color_index")
 
         if (color_name is None) and (color_index is None):
-            return values
+            return v
 
-        if color_name is None:
+        elif color_name is None:
             raise ValueError("'color_name' can't be null when 'color_index' is not null")
 
-        if color_index is None:
+        elif color_index is None:
             raise ValueError("'color_index' can't be null when 'color_name' is not null")
 
-        # Update color
-        values["color"] = get_colors((color_name, color_index))
-
-        return values
-
-    @root_validator
-    def round_only_if_background(cls, values):
-
-        if not values.get("round"):
-            return values
-
-        for x in ["color", "background"]:
-            if values.get(x):
-                return values
-
-        raise ValueError(f"Round is only allowed when there is background")
+        return get_colors((color_name, color_index))
 
 
-class Job:
-    """Use for doing multiple transformations"""
+class AddBackgroundColor(AddColorGeneric):
+    action: Literal["add_background_color"]
+    suffix: str = None
 
-    def __init__(self, path_in, job_config, reprocess=True):
+    @validator("suffix", pre=True, always=True)
+    def populate_suffix(cls, v, values):
+        """Infer color form color_name and color_index"""
 
-        self.job_config = job_config
+        color_name = values.get("color_name")
+        color_index = values.get("color_index")
+        color = values.get("color")
 
-        self.folder_out, self.base_name = get_folder_out_and_name(path_in)
-        self.image_in = get_image(path_in)
+        if color_name and color_index:
+            return f"__{color_name}_{color_index}"
 
-        self.reprocess = reprocess
+        elif color:
+            return f"__{color}"
 
-    @property
-    def path_out(self):
-        """Infer out name based on the config"""
-        out = f"{self.folder_out}/{self.base_name}"
+        raise ValueError("Invalid color configuration")
 
-        format_out = ".jpg"
+    def run(self, image_in):
+        return add_background_color(image_in, background_color=self.color)
 
-        if self.job_config.color_name or self.job_config.color_index:
-            out += f"__{self.job_config.color_name}_{self.job_config.color_index}"
-        else:
-            if self.job_config.color:
-                out += f"__{self.job_config.color}"
 
-        if self.job_config.background and not self.job_config.color:
-            out += "_no_bg"
-            format_out = ".png"
+class ToGrayscale(AddColorGeneric):
+    action: Literal["to_grayscale"]
+    suffix: str = None
 
-        if self.job_config.round:
-            out += "_round"
-            format_out = ".png"
+    @validator("suffix", pre=True, always=True)
+    def populate_suffix(cls, v, values):
+        """Infer color form color_name and color_index"""
 
-        if self.job_config.grayscale:
-            out += "_grayscale"
+        color_name = values.get("color_name")
+        color_index = values.get("color_index")
+        color = values.get("color")
 
-        return out + format_out
+        if color_name and color_index:
+            return f"_grayscale__{color_name}_{color_index}"
+
+        elif color:
+            return f"_grayscale__{color}"
+
+        return "_grayscale"
+
+    def run(self, image_in):
+        return to_grayscale(image_in, background_color=self.color)
+
+
+class Job(BaseModel):
+    path_in: str
+    folder_out: str
+
+    transformations: list[Union[RoundImage, RemoveBg, AddBackgroundColor, ToGrayscale]]
+    name_out: str = None
+    extension: str = None
+    path_out: str = None
+    reprocess: bool = False
+
+    @validator("extension", pre=True, always=True)
+    def populate_extension(cls, v, values):
+        transformations = values.get("transformations")
+        last_transformation = transformations[-1]
+
+        if isinstance(last_transformation, RoundImage) or isinstance(last_transformation, RemoveBg):
+            return "png"
+        return "jpg"
+
+    @validator("name_out", pre=True, always=True)
+    def populate_name_out(cls, v, values):
+        transformations = values.get("transformations")
+        path_in = values.get("path_in")
+
+        out = path_in.split("/")[-1].rsplit(".", maxsplit=1)[0]
+        for x in transformations:
+            if x.action == "remove_bg":
+                if len(transformations) == 0:
+                    return x.suffix
+
+            else:
+                out += x.suffix
+
+        return out
+
+    @validator("path_out", pre=True, always=True)
+    def populate_path_out(cls, v, values):
+        folder_out = values.get("folder_out").rstrip("/")
+        name_out = values.get("name_out")
+        extension = values.get("extension")
+
+        return f"{folder_out}/{name_out}.{extension}"
 
     @property
     def needs_reprocessing(self):
         return self.reprocess or not os.path.exists(self.path_out)
 
-    def transform(self):
-        """Aply all asked transformations"""
-
-        if not self.needs_reprocessing:
-            self.image_out = get_image(self.path_out)
-            return False
-
-        image = self.image_in
-
-        if not self.job_config.background:
-            image = remove_bg(image)
-
-        if self.job_config.grayscale:
-            image = to_grayscale(image, background_color=self.job_config.color)
-
-        elif self.job_config.color:
-            image = add_background_color(image, self.job_config.color)
-
-        if self.job_config.round:
-            image = round_image(image)
-
-        self.image_out = image
-        return True
-
-    def export(self):
-        """Export result"""
-
-        if self.path_out.endswith(".jpg"):
-            self.image_out = self.image_out.convert("RGB")
-
-        if self.needs_reprocessing:
-            self.image_out.save(self.path_out)
-
     def process(self):
-        """Transform and export"""
-        self.transform()
-        self.export()
+        if not self.needs_reprocessing:
+            return get_image(self.path_out)
+
+        image = get_image(self.path_in)
+
+        for transformation in self.transformations:
+            image = transformation.run(image)
+
+        if self.extension in ("jpg", "jpeg"):
+            image = image.convert("RGB")
+
+        if self.reprocess:
+            image.save(self.path_out)
+        return image
